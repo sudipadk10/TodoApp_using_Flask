859cefb3c44fc99f66f44acc8e99781ed7eacb52
sudipadk10 <adhikarisudip869@gmail.com>
2025-06-23 20:37:59 +0545

    delete todo function added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,redirect,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -13,18 +13,36 @@ class Todo(db.Model):
     sn = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
         return f'{self.sn} -> {self.title}'
 
-@app.route('/')
+@app.route('/' , methods = ['GET', 'POST'])
 def index():
-    todo = Todo(title="web development" , desc = "learn flask for now then dive into django")
-    db.session.add(todo)
+    if request.method == 'POST':
+        print("posted bro")
+        title = request.form['title']
+        desc = request.form['desc']
+        
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit() 
+    all_todo = Todo.query.all()
+    return render_template('index.html', all_todo = all_todo)
+
+@app.route('/delete/<int:s>')
+def delete(s):
+    todo = Todo.query.filter_by(sn=s).first()
+    db.session.delete(todo)
     db.session.commit()
+    return redirect('/')
     
-    return render_template('index.html')
+
+# @app.route('/update/<int:s>')
+# def update(s):
+#     todo = Todo.query.filter_by(sn=s).first()
+#     return render_template('update.html', todo = todo)
 
 
 
